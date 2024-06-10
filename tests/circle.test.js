c9bfb129dec8c10d5f76907a5019c7be3e9ad3b5
Ryan Petersen <rp26898@gmail.com>
2024-06-09 21:19:04 -0700

    debugged tests to work correctly

--- tests/circle.test.js
+++ tests/circle.test.js
@@ -4,5 +4,5 @@ test('Circle render method with color blue', () => {
     const shape = new Circle(); // create a new Square instance
     shape.setColor("blue"); // set the color to blue
     // check if the rendered SVG matches the expected output
-    expect(shape.render().replace(/\s+/g, ' ').trim()).toEqual('<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"> <circle cx="150" cy="100" r="100" fill="blue"/> <text x="150" y="125" font-size="70 text-anchor="middle" fill=""></text> </svg>');
+    expect(shape.render().replace(/\s+/g, ' ').trim()).toEqual('<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"> <circle cx="150" cy="100" r="100" fill="blue"/> <text x="150" y="125" font-size="70" text-anchor="middle" fill=""></text> </svg>');
 });
---
+++ tests/triangle.test.js
@@ -0,0 +1,8 @@
+const Triangle = require('../lib/triangle.js'); // This imports the Triangle class
+
+test('Triangle render method with color blue', () => {
+    const shape = new Triangle(); // This create a new Triangle instance
+    shape.setColor("blue"); // This set the color to blue
+    // This checks if the rendered SVG matches the expected output
+    expect(shape.render().replace(/\s+/g, ' ').trim()).toEqual('<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"> <polygon points="100, 15 200, 200 0, 200" fill="blue"/> <text x="100" y="165" font-size="60" text-anchor="middle" fill=""></text> </svg>')
+});
